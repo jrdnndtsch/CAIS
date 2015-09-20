758dd3e452ab019e173a41ed29f74f95eafb8972
deartrudence <trudy.macnabb@gmail.com>
2015-09-20 16:12:25 -0400

    spelling correction

--- app/models/academic.rb
+++ app/models/academic.rb
@@ -1,5 +1,5 @@
 class Academic < ActiveRecord::Base
-	SUBJECT = [ 'Computer Studies', 'Advanced/Enrichment_programs', 'English', 'Science', 'Humanities/Social Sciences', 'Support Programs', 'Special Programs', 'Performing/Visual Arts', 'Music', 'Foriegn Languages', 'History', 'Mathematics', 'Religion/Philosophy', 'Distinctive or New Programs' ]
+	SUBJECT = [ 'Computer Studies', 'Advanced/Enrichment Programs', 'English', 'Science', 'Humanities/Social Sciences', 'Support Programs', 'Special Programs', 'Performing/Visual Arts', 'Music', 'Foriegn Languages', 'History', 'Mathematics', 'Religion/Philosophy', 'Distinctive or New Programs' ]
 	has_many :school_academics
 	has_many :schools, through: :school_academics
 	belongs_to :academic_type
